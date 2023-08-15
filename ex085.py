0748e842da00d3d443672af3f0d8b809d747382d
ruannino <ruanfariasdc@hotmail.com>
2023-08-14 22:21:42 -0300

    Fourth Commit 14.08.2023

--- ex085.py
+++ ex085.py
@@ -1,3 +1,17 @@
 # Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única
 # que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
+numbers = [[], []]
+for n in range(0, 7):
+    valor = int(input('Digite um valor: '))
+
+    if valor % 2 == 0:
+        numbers[0].append(valor)
+    else:
+        numbers[1].append(valor)
+
+numbers[0].sort()
+numbers[1].sort()
+
+print(f'Os válores pares em ordem crescente é {numbers[0]}')
+print(f'Os válores ímpares em ordem crescente é {numbers[1]}')
--- ex086.py
+++ ex086.py
@@ -1,2 +1,14 @@
 # Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
 # No final, mostre a matriz na tela, com a formatação correta.
+#         0,0-0,1-0,2    1,0-1,1-1,2   2,0-2,1-2,2
+matriz = []
+for e in range(3):
+    linha = []
+    for i in range(3):
+        valor = int(input(f'Digite o valor para posição {(e, i)}: '))
+        linha.append(valor)
+    matriz.append(linha)
+for linha in matriz:
+    for valor in linha:
+        print(f'[{valor:^5}]', end='')
+    print()
