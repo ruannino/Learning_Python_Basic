ca1e0edb53ad5d1fa7bab06c883a1a83cc62a269
ruannino <ruanfariasdc@hotmail.com>
2023-08-16 18:22:28 -0300

    Sixth Commit 16.08.2023

--- ex090.py
+++ ex090.py
@@ -1,2 +1,15 @@
 # Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
 # No final, mostre o conteúdo da estrutura na tela.
+boletim = dict()
+boletim['aluno'] = str(input('Digite o nome: '))
+boletim['media'] = float(input(f'Digite a média de {boletim["aluno"]}: '))
+if boletim['media'] >= 7:
+    boletim['situação'] = 'Aprovado'
+elif 5 <= boletim['media'] >= 5:
+    boletim['situação'] = 'Recuperação'
+else:
+    boletim['situação'] = 'Reprovado'
+print(F'{"BOLETIM":^20}')
+print('=' * 20)
+for k, v in boletim.items():
+    print(f'{k:<10} : {v:<10}')
--- ex092.py
+++ ex092.py
@@ -1,3 +1,28 @@
 # Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário
 # se for o caso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
 # Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar. OBS: considerar 35 anos contribuição.
+from datetime import date
+ficha = {}
+while True:
+    ficha['nome'] = str(input('Nome: '))
+    ano_de_nascimento = int(input('Ano de Nascimento: '))
+    idade = date.today().year - ano_de_nascimento
+    ficha['idade'] = idade
+    n_trabalho = int(input('Carteirta de Trabalho (0 não tem): '))
+    if n_trabalho == 0:
+        break
+    else:
+        ficha['CTPS'] = n_trabalho
+        ano_contratacao = int(input('Ano de Contratação: '))
+        contract = date.today().year - ano_contratacao
+        ficha['Ano de Contratação'] = ano_contratacao
+        ficha['Salário'] = float(input('Salário: R$ '))
+        aposentadoria = (35 - contract) + idade
+        ficha['Aposentadoria'] = aposentadoria
+        break
+print('=-' * 15)
+print('{:=^30}'.format('FICHA'))
+print('=-' * 15)
+for k, v in ficha.items():
+    print(f'- {k} tem o valor {v}')
+
