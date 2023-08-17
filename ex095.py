5fcf0a016eaa53453915ba1fd82af867613a7750
ruannino <ruanfariasdc@hotmail.com>
2023-08-17 20:59:08 -0300

    Seventh Commit 17.08.2023

--- ex095.py
+++ ex095.py
@@ -4,17 +4,25 @@
 # Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e
 # quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será
 # guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
-somag = 0
-jogador = dict()
-jogador['nome'] = str(input('Nome do jogador: '))
-n = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
-gols = list()
-for g in range(0, n):
-    gols.append(int(input(f'Quantos gols o na partida {g + 1}: ')))
-    somag += gols[g]
-jogador['partidas'] = n
-jogador['gols'] = gols
-jogador['total'] = somag
+time = []
+jogador = {}
+gols = []
+while True:
+    jogador['nome'] = str(input('Nome do jogador: '))
+    jogador['partidas'] = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    for g in range(0, jogador['partidas']):
+        gols.append(int(input(f'Quantos gols o na partida {g + 1}: ')))
+    jogador['gols'] = gols[:]
+    jogador['total'] = sum()
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar ? [S/N] :')).upper()[0]
+        if resp in 'SN':
+            break
+        print('\033[31mErro!\033[m digite S ou N...')
+    if resp == 'N':
+        break
+
 print('=-' * 15)
 print('{:^30}'.format('TABELA'))
 print('=-' * 15)
@@ -25,3 +33,5 @@ for p, gp in enumerate(jogador['gols']):
 print()
 print(f'Foi um total de {jogador["total"]} gols. ')
 print('=-' * 15)
+
+print(time)
