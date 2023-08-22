8e1511b8fa72a57e85330c42a17fbcf8af5f285e
ruannino <ruanfariasdc@hotmail.com>
2023-08-21 23:55:31 -0300

    Eleventh Commit 21.08.2023

---
+++ ex038.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia dois números inteiros e compare-os,
+# mostrando na tela uma mensagem:
+# - o primeiro valor é maior
+# - o segundo valor é maior
+# - não existe valor maior; os dois são iguais
--- ex102.py
+++ ex102.py
@@ -2,3 +2,4 @@
 # a calcular e o outro chamado show, que será um valor lógico(opcional) indicando se será mostrado ou não na tela o
 # processo de cálculo do fatorial.
 # print(fatorial(5, show=True))   help(fatorial)
+
