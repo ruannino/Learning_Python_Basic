97e30a514a3f99f56b7e872441768e81f35a2be7
ruannino <ruanfariasdc@hotmail.com>
2023-08-22 22:08:58 -0300

    Twelfth Commit 22.08.2023

--- ex102.py
+++ ex102.py
@@ -3,3 +3,24 @@
 # processo de cálculo do fatorial.
 # print(fatorial(5, show=True))   help(fatorial)
 
+
+def fatorial(num, show=False):
+    """
+    -> Funçao calcula o fatorial de um número e exibe o resultado na tela.
+    :param num: recebe o valor do número.
+    :param show: parâmetro opcional para exibir ou não todo o calculo
+    :return: retorna o fatorial de um número na tela.
+    """
+    f = 1
+    for n in range(num, 0, -1):
+        if show:
+            print(f'{n}', end='')
+            if n > 1:
+                print(' * ', end='')
+            else:
+                print(' = ', end='')
+        f *= n
+    print(f'{f}')
+
+
+fatorial(5, show=True)
