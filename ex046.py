af9c21c4fee5175399ee885d8208a52afb38df42
ruannino <ruanfariasdc@hotmail.com>
2023-08-22 23:08:36 -0300

    Twelfth Commit 22.08.2023

---
+++ ex046.py
@@ -0,0 +1,4 @@
+# Faça um programa que mostre na tela
+# Uma contagem regressiva para o estouro e fogos de artifício,
+# Indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+# USANDO LOOP AGORA
---
+++ ex048.py
@@ -0,0 +1,2 @@
+# Faça um programa que calcule a soma entre todos os números impares
+# que são múltiplos de três (3) e que se encontram no intervalo de 1 até 500.
