5ab9c938f86fd21984bdd7f021cff5ca63d48378
ruannino <ruanfariasdc@hotmail.com>
2023-08-23 22:37:29 -0300

    Thirteenth Commit 23.08.2023

---
+++ ex053.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia uma frase qualquer
+# E diga se ela é um palíndromo, desconsiderando os espaços'''
+# Após a sopa
+# A sacada da casa
+# A torre da derrota
+# o lobo ama o bolo
+# Anotaram a data da maratona
--- ex096.py
+++ ex096.py
@@ -1,9 +1,9 @@
 # Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e
 # comprimento) e mostre a área do terreno.
 def area(larg, comp):
-    s = larg * comp
+    r = larg * comp
     print(f'A área do terreno com {larg:.2f}x{comp:.2f}.'.center(45))
-    print(f'Total de área de {s}m².'.center(45))
+    print(f'Total de área de {r}m².'.center(45))
 
 
 print('=-' * 23)
