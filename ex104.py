5a6fa32680e87aa4649a8f373ec7900673a7502d
ruannino <ruanfariasdc@hotmail.com>
2023-08-26 19:16:06 -0300

    Sixteenth Commit 26.08.2023

--- ex104.py
+++ ex104.py
@@ -1,11 +1,19 @@
 # Crie um programa que tenha uma função chamada leiaInt(), que vai funcionar semelhante a função input() do Python, só
 # que fazendo a validação para aceitar apenas um valor numérico.    Ex.: n = leiaInt('Digite um n')
-def leiaInt(i):
-    while i not in int:
-        print('Erro! digite um número inteiro!')
+def leia_int(msn):
+    valor = 0
+    ok = False
+    while True:
+        num = str(input(msn))
+        if num.isnumeric():
+            valor = num
+            ok = True
+        else:
+            print('\033[31mErro! Digite um número inteiro válido...\033[m')
+        if ok:
+            break
+    return valor
 
 
-
-
-
-leiaInt()
+n = leia_int('Digite um número: ')
+print(f'Você digitou o número {n}.')
--- ex106.py
+++ ex106.py
@@ -1,2 +1,42 @@
-# Faça um mini-sistema que utilize o INTERACTIVE HELP do Python. O usuário vai digitar o comando e o manual vai aparecer.
-# Quando o usuário digitar a palavra 'FIM', o programa se encerrará.    OBS.: use cores.
+# Faça um mini-sistema que utilize o INTERACTIVE HELP do Python. O usuário vai digitar o comando e o manual vai
+# aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará.    OBS.: use cores.
+from time import sleep
+c = ('\033[m',    # [0] sem cor
+     '\033[41m',  # [1] vermelha
+     '\033[42m',  # [2] verde
+     '\033[43m',  # [3] amarelo
+     '\033[44m',  # [4] azul
+     '\033[45m',  # [5] roxo
+     '\033[40m'   # [6] preto
+     )
+
+
+def ajuda(com):
+    sleep(1)
+    titulo(f'Acessando manual do comando {com}', 4)
+    print(c[6])
+    sleep(1)
+    help(com)
+    print(c[0])
+
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+
+
+# Programa Principal
+comando = ''
+while True:
+    titulo('SISTEMA DE AJUDA PyHELP', 2)
+    comando = str(input("Função ou Biblioteca > "))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+sleep(0.5)
+titulo('ATÉ LOGO!', 1)
