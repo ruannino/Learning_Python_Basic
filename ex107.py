181e7275ba77ed41365c98e8513828bcf2f41327
ruannino <ruanfariasdc@hotmail.com>
2023-08-27 02:57:01 -0300

    Seventeenth Commit 27.08.2023

--- ex107.py
+++ ex107.py
@@ -1,3 +1,12 @@
 # Crie um módulo chamado moeda.py que tenha as funções incorporadas 'aumentar()', 'diminuir()', 'dobro()' e 'metade()'.
 # Faça também um programa que importe esse módulo e use algumas dessas funções.
 # Obs.: por exemplo, o 'aumentar()' recebe o preço e uma porcentagem, e calcula.
+from moedas import moeda
+
+v = float(input('Digite o valor do produto: R$ '))
+p = int(input('Digite a porcentagem a incrementar: % '))
+print(f'O valor do produto é {v} com incremento de {p}% tem o valor final de R${moeda.aumentar(v, p)}')
+print(f'O valor do produto é {v} com desconto de {p}% tem o valor final de R${moeda.diminuir(v, p)}')
+print(f'O dobro do valor é R${moeda.dobro(v)}')
+print(f'A metade do valor é R${moeda.metade(v)}')
+print(f'{moeda.moeda(v)}')
--- ex108.py
+++ ex108.py
@@ -1,2 +1,7 @@
 # Adapte o código do desafio 107, criando uma função adicional chamada 'moeda()' que consiga mostrar os valores
 # como um valor monetário formatado.
+from moedas import moeda
+
+v = float(input('Digite o valor do produto: R$ '))
+p = int(input('Digite a porcentagem a incrementar: % '))
+print(f'{moeda.moeda(v)}')
