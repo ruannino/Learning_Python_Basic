f2412c8aecbabf85c79a6b26ce9efb17c61999d9
ruannino <ruanfariasdc@hotmail.com>
2023-08-27 19:28:13 -0300

    Seventeenth Commit 27.08.2023

--- ex113.py
+++ ex113.py
@@ -1,3 +1,10 @@
 # Reescreva a função leia_int() que fizemos no desafio 104, incluindo agora a possibilidade
 # da digitação de um número de tipo inválido. Aproveite e crie também uma função leia_float()
 # com a mesma funcionalidade.
+from utilidadesCeV.dado import leia_int, leia_float
+
+n = leia_int('Digite um número Inteiro: ')
+print(f'o valor digitado foi {n}')
+
+f = leia_float('Digite um número Real: ')
+print(f'o valor digitado foi {f}')
--- utilidadesCeV/dado/__init__.py
+++ utilidadesCeV/dado/__init__.py
@@ -7,3 +7,31 @@ def leia_dinheiro(msg):
         else:
             valido = True
             return float(entrada)
+
+
+def leia_int(msn):
+    while True:
+        try:
+            num = int(input(msn))
+        except (ValueError, TypeError):
+            print('\033[31mErro! Digite um número Inteiro válido...\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\033[31mEntrada de dados interrompida pelo usuário.\033[m')
+            return 0
+        else:
+            return num
+
+
+def leia_float(msn):
+    while True:
+        try:
+            num = float(input(msn))
+        except (ValueError, TypeError):
+            print('\033[31mErro! Digite um número Real válido...\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\033[31mEntrada de dados interrompida pelo usuário.\033[m')
+            return 0
+        else:
+            return num
