83ce645fa9c3c6a1004e42785ef906e8533dcd66
ruannino <ruanfariasdc@hotmail.com>
2023-09-07 19:44:23 -0300

    Twenty Seventh Commit 07.09.2023

--- ex023.py
+++ ex023.py
@@ -1,1 +1,11 @@
 # Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados. ex.: digite um número: 1834
+numero = input('Digite um número entre [0-9999]: ')
+numero_formatado = numero.zfill(4)
+milhar = numero_formatado[0]
+centena = numero_formatado[1]
+dezena = numero_formatado[2]
+unidade = numero_formatado[3]
+print(f'''O milhar: {milhar}
+A centena: {centena}
+A dezena: {dezena}
+A unidade: {unidade}''')
