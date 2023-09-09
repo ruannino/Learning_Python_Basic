f9a7eff1aa1af353a4c1f6683f02123a11852bd0
ruannino <ruanfariasdc@hotmail.com>
2023-09-09 20:36:17 -0300

    Twenty ninth Commit 09.09.2023

--- ex035.py
+++ ex035.py
@@ -1,4 +1,8 @@
-# Desenvolva um programa que leia o comprimento de três retas
-# e diga ao usuário se elas podem ou não formar um triângulo
-# Inclusive posso dizer qual tipo de triângulo pode ser formado.
-# Não deve ser difícil isso em Python.
+# Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo
+r1 = float(input('Coprimento da 1º linha: '))
+r2 = float(input('Coprimento da 2º linha: '))
+r3 = float(input('Coprimento da 3º linha: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print(f'A soma das retas podem formar um \033[32mTRIÂNGULO!\033[m')
+else:
+    print(f'A soma das retas \033[31mNÃO\033[m podem formar um \033[31mTRIÂNGULO!\033[m')
