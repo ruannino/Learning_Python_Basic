fe1e6a9329522eef519fe3a11dfd44113a2160b4
ruannino <ruanfariasdc@hotmail.com>
2023-09-09 20:22:56 -0300

    Twenty ninth Commit 09.09.2023

--- ex032.py
+++ ex032.py
@@ -1,1 +1,9 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
+from datetime import date
+ano = int(input('Qual ano você quer analisar? Coloque 0 para analisar o ano atual: '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano} é \033[32mBISSEXTO\033[m!')
+else:
+    print(f'O ano {ano} \033[31mNÃO\033[m é \033[31mBISSEXTO\033[m!')
--- ex033.py
+++ ex033.py
@@ -6,6 +6,13 @@ n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
 maior = menor = n1
-if n2 > n1:
+if n2 > n1 and n3 < n2:
     maior = n2
-elif
+if n3 > n2 and n3 > n1:
+    maior = n3
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n2 and n3 < n1:
+    menor = n2
+print(f'O \033[33mMENOR\033[m número digitado foi \033[33m{menor}\033[m, '
+      f'e o \033[33mMAIOR\033[m foi \033[33m{maior}\033[m')
