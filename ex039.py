6b23b7f8c509787ad11eb6ea871ef767603ede93
ruannino <ruanfariasdc@hotmail.com>
2023-09-16 20:58:17 -0300

    Thirty Third Commit 16.09.2023

--- ex039.py
+++ ex039.py
@@ -7,10 +7,10 @@ from datetime import date
 nascimento = int(input('Ano de nascimento: '))
 idade = date.today().year - nascimento
 if idade == 18:
-    print(f'Um jovem de {idade} anos. Deve se alistar imediatamente!')
+    print(f'Um jovem de \033[32m{idade}\033[m anos. Deve se alistar imediatamente!')
 elif idade > 18:
     diferenca_maior = idade - 18
-    print(f'Com {idade} anos. Já passou {diferenca_maior} anos do seu alistamento!')
+    print(f'Com \033[31m{idade}\033[m anos. Já passou \033[31m{diferenca_maior}\033[m anos do seu alistamento!')
 else:
     diferenca_menor = 18 - idade
-    print(f'Com {idade} anos. Faltam {diferenca_menor} anos para o seu alistamento!')
+    print(f'Com \033[35m{idade}\033[m anos. Faltam \033[35m{diferenca_menor}\033[m anos para o seu alistamento!')
