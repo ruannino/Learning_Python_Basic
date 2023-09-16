ab01162b1acccb0ce9b59407b69785f2d19e4923
ruannino <ruanfariasdc@hotmail.com>
2023-09-15 22:17:38 -0300

    Thirty Second Commit 15.09.2023

--- ex043.py
+++ ex043.py
@@ -5,3 +5,18 @@
 # - 25 até 30: sobrepeso;
 # - 30 até 40: obesidade;
 # - acima de 40: obesidade mórbida.
+peso = float(input('Informe o peso: '))
+altura = float(input('Informe a altura: '))
+IMC = peso / (altura ** 2)
+if IMC < 18.5:
+    print(f'Seu IMC \033[34m{IMC:.2f}\033[m. Você está abaixo do peso.')
+elif 18.5 <= IMC <= 24.9:
+    print(f'\033[32mSeu IMC {IMC:.2f}. Você está saudável.\033[m')
+elif 25 <= IMC <= 29.9:
+    print(f'\033[35mSeu IMC {IMC:.2f}. Você está com sobrepeso.\033[m')
+elif 30 <= IMC <= 34.9:
+    print(f'\033[33mSeu IMC {IMC:.2f}. Você está com obesidade grau I.\033[m')
+elif 35 <= IMC <= 39.9:
+    print(f'\033[33mSeu IMC {IMC:.2f}. Você está com obesidade grau II (severa).\033[m')
+elif IMC >= 40:
+    print(f'\033[31mSeu IMC {IMC:.2f}. Você está com obesidade grau III (mórbida).\033[m')
