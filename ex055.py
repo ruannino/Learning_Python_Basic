40eff5e76a2776c6c9d2d1fed81f29cd2a34f91e
ruannino <ruanfariasdc@hotmail.com>
2023-09-18 20:05:13 -0300

    Thirty Fifth Commit 18.09.2023

--- ex055.py
+++ ex055.py
@@ -1,1 +1,14 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos
+menor = maior = 0
+for c in range(1, 6):
+    peso = float(input(f'Digite o peso da \033[32m{c}º\033[m pessoa: kg'))
+    if c == 1:
+        menor = peso
+        maior = peso
+    else:
+        if peso > maior:
+            maior = peso
+        elif peso < menor:
+            menor = peso
+print(f'O \033[31mmaior\033[m peso foi de \033[31m{maior:.2f}\033[mKg.'
+      f'\nO \033[33mmenor\033[m peso foi de \033[33m{menor:.2f}\033[mKg.')
--- ex056.py
+++ ex056.py
@@ -2,3 +2,20 @@
 # - A média de idade do grupo
 # - Qual é o nome do homem mais velho
 # - Quantas mulheres têm menos de 20 anos
+mais_velho = soma = mais_nova = 0
+homem_velho = ''
+for c in range(1, 5):
+    nome = str(input(f'Digite o nome da {c}º pessoa: '))
+    idade = int(input(f'Digite a idade de {nome}: '))
+    sexo = str(input(f'Digite o sexo de {nome}: \033[30m[M/F]\033[m')).strip().upper()
+    soma += idade
+    if 'F' in sexo[0] and idade < 20:
+        mais_nova += 1
+    elif idade > mais_velho and 'M' in sexo[0]:
+        mais_velho = idade
+        homem_velho = nome
+media = soma / 4
+print(f'As informações gerais do grupo:'
+      f'\nA média de idade do grupo é de \033[32m{media:.2f}\033[m anos.'
+      f'\nO homem mais venho se chama \033[32m{homem_velho}\033[m.'
+      f'\nExistem \033[32m0{mais_nova}/033[m mulher(es) com menos de 20 anos.')
