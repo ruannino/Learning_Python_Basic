a61ec546acf20d533786145d6752e72d89856d91
ruannino <ruanfariasdc@hotmail.com>
2023-09-20 23:40:13 -0300

    Thirty Seventh Commit 20.09.2023

--- ex059.py
+++ ex059.py
@@ -5,33 +5,40 @@
 # 4: novos números
 # 5: sair do programa
 # Seu programa deverá realizar a operação solicitada em cada caso:
-opcao = 4
-print('=' * 40)
+from time import sleep
+opcao = 0
 print('=' * 40)
 num1 = int(input('Digite o primeiro número: '))
 num2 = int(input('Digite o segundo número: '))
 while opcao != 5:
     print('Escolha uma opção para realizar!')
-    opcao = int(input('[1] Somar:'
-                      '\n[2] Multiplicar:'
-                      '\n[3] Maior:'
-                      '\n[4] Novos Números:'
-                      '\n[5] Sair do Programa:'))
+    opcao = int(input('\033[40m[1]\033[m Somar:'
+                      '\n\033[40m[2]\033[m Multiplicar:'
+                      '\n\033[40m[3]\033[m Maior:'
+                      '\n\033[40m[4]\033[m Novos Números:'
+                      '\n\033[40m[5]\033[m Sair do Programa:'))
     print('=' * 40)
     if opcao == 1:
-        print(f'SOMAR:  {num1} + {num2} = {num1 + num2}.')
+        print(f'\033[32mSOMAR: \033[m{num1} + {num2} = {num1 + num2}.')
         print('=' * 40)
     elif opcao == 2:
-        print(f'MULTIPLICAR:  {num1} + {num2} = {num1 + num2}.')
+        print(f'\033[32mMULTIPLICAR: \033[m{num1} x {num2} = {num1 * num2}.')
         print('=' * 40)
     elif opcao == 3:
         if num1 > num2:
-            print(f'MAIOR:  {num1} > {num2}, logo {num1} é o MAIOR.')
+            print(f'\033[32mMAIOR:\033[m {num1} > {num2}, logo {num1} é o MAIOR.')
             print('=' * 40)
         else:
-            print(f'MAIOR:  {num2} > {num1}, logo {num2} é o MAIOR.')
+            print(f'\033[32mMAIOR:\033[m {num2} > {num1}, logo {num2} é o MAIOR.')
             print('=' * 40)
     elif opcao == 4:
         num1 = int(input('Digite o primeiro número: '))
         num2 = int(input('Digite o segundo número: '))
+    elif opcao == 5:
+        print('Preparando para sair...', end='')
+        sleep(1)
+    else:
+        print('\033[31mERRO! Opção inválida, tente novamente...\033[m')
+        print('=' * 40)
+    sleep(1)
 print('\033[31mENCERRANDO...\033[m')
--- ex060.py
+++ ex060.py
@@ -1,4 +1,3 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
 # exemplo: 5! = 5 * 4 * 3 * 2 * 1 = 120
-num = int(input('Digite o número: '))
-while fatorial
+num = int(input('Digite um número: '))
