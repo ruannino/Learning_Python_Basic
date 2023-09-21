7709650a522a7a4b55912c046bd70c85d6b019cb
ruannino <ruanfariasdc@hotmail.com>
2023-09-20 23:55:58 -0300

    Thirty Seventh Commit 20.09.2023

--- ex060.py
+++ ex060.py
@@ -1,3 +1,26 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
 # exemplo: 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+# Método com biblioteca math
+from math import factorial
 num = int(input('Digite um número: '))
+f = factorial(num)
+print(f'O fatorial de {num}! é {f}.')
+
+# Método Extensivo
+n = int(input('Digite um número: '))
+c = n
+m = c
+while c > 0:
+    if n == c:
+        print(f'{n}! = {c} x ', end='')
+        c -= 1
+    elif c > 1 and c != n:
+        print(f'{c} x ', end='')
+        m *= c
+        c -= 1
+    else:
+        print(f'{c} = ', end='')
+        m *= c
+        c -= 1
+print(f'{m}')
