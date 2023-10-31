d3110341ba8f50f2f94d6a048b1c8d365860a029
ruannino <ruanfariasdc@hotmail.com>
2023-10-31 20:59:30 -0300

    Forty Fifth Commit 31.10.2023

--- Mundo_3/ex072.py
+++ Mundo_3/ex072.py
@@ -2,7 +2,6 @@
 # Seu programa deverá ler um número pelo teclado ( entre 0 e 20) e mostrá-lo por extenso.
 extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
            'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-
 extenso_ingles = ('Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
                   'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen',
                   'Nineteen', 'Twenty')
