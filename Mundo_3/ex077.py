30407229c63da409796e7cd24914f5b40c232950
ruannino <ruanfariasdc@hotmail.com>
2023-11-01 21:22:00 -0300

    Forty Sixth Commit 01.11.2023

--- Mundo_3/ex077.py
+++ Mundo_3/ex077.py
@@ -1,5 +1,10 @@
 # Crie um programa que tenha uma tupla com várias palavras(não usar acentos).
 # Depois mostrar, para cada palavra, quais são as suas vogais.
 
-words = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis',
-         'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+conjunto_palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis',
+                     'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+for palavra in conjunto_palavras:
+    print(f'\nNa palavra {palavra.upper()} temos as vogais ', end='')
+    for letra in palavra:
+        if letra in 'aeiou':
+            print(f'{letra}', end=' ')
