d182f462a5767d1258e9d599fc5334d10ba2e543
ruannino <ruanfariasdc@hotmail.com>
2023-11-02 20:29:31 -0300

    Forty Seventh Commit 02.11.2023

--- Mundo_3/ex083.py
+++ Mundo_3/ex083.py
@@ -1,15 +1,16 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo
 # deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
-exp = []
+exp = input('Digite sua expressão: ')
 sim = []
-exp.append(str(input('Digite sua expressão: ')).strip())
-for ex in exp:
-    for e in ex:
-        if e == '(':
+for e in exp:
+    if e == '(':
+        sim.append(e)
+    elif e == ')':
+        if not sim:
             sim.append(e)
-        elif e == ')':
-            sim.append(e)
-if len(sim) % 2 == 0:
-    print(f'A expressão {exp}. VÁLIDA!')
+        elif sim[-1] == '(':
+            sim.pop()
+if not sim:
+    print(f'A expressão \033[33m{exp}\033[m é \033[32mVÁLIDA!\033[m')
 else:
-    print(f'A expressão {exp}. INVÁLIDA!')
+    print(f'A expressão \033[33m{exp}\033[m é \033[31mINVÁLIDA!\033[m')
