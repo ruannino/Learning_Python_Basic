045deac9c8902f20f2358ed88108f321d2fc73f6
ruannino <ruanfariasdc@hotmail.com>
2023-11-02 19:44:12 -0300

    Forty Seventh Commit 02.11.2023

--- Mundo_3/ex080.py
+++ Mundo_3/ex080.py
@@ -2,6 +2,20 @@
 # já na posição correta de inserção (sem usar o sort()). No final mostre a lista ordenada na tela.
 numbers = []
 for n in range(0, 5):
-
-
-print(f'{numbers}')
+    v = int(input('Digite um valor: '))
+    if n == 0 or v >= numbers[-1]:
+        numbers.append(v)
+        print('Número adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(numbers):
+            if v <= numbers[pos]:
+                numbers.insert(pos, v)
+                print(f'O número foi adicionado a posição {pos} da lista.')
+                break
+            pos += 1
+print('\033[35m=\033[m' * 40)
+print('NÚMEROS EM ORDEM CRESCENTE'.center(40))
+print('\033[35m=\033[m' * 40)
+print(f'{numbers}'.center(40))
+print('\033[35m-\033[m' * 40)
