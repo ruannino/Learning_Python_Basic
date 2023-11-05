25cff790f7ff51b524f5a580053e80ca8558d8c5
ruannino <ruanfariasdc@hotmail.com>
2023-11-05 09:53:56 -0300

    Forty Ninth Commit 05.11.2023

--- Mundo_3/ex090.py
+++ Mundo_3/ex090.py
@@ -1,2 +1,17 @@
 # Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
 # No final, mostre o conteúdo da estrutura na tela.
+aluno = dict()
+aluno['Nome'] = str(input('Nome do aluno: ')).strip()
+aluno['Média'] = float(input('Média do aluno: ').replace(',', '.'))
+if aluno['Média'] >= 7:
+    aluno['Situação'] = '\033[32mAprovado\033[m'
+elif 5 <= aluno['Média'] < 7:
+    aluno['Situação'] = 'em \033[33mRecuperação\033[m'
+elif aluno['Média'] < 5:
+    aluno['Situação'] = '\033[31mReprovado\033[m'
+print('\033[35m=\033[m' * 30)
+print(f'{aluno["Nome"]:^30}')
+print('\033[35m=\033[m' * 30)
+print(f'\nMédia --> {aluno["Média"]:.1f}')
+print(f'\nO aluno está {aluno["Situação"]}.')
+print('\033[35m=\033[m' * 30)
