d70c691881eead7981502052abaa3b95f3562a4e
ruannino <ruanfariasdc@hotmail.com>
2023-11-05 18:44:20 -0300

    Forty Ninth Commit 05.11.2023

--- Mundo_3/ex097.py
+++ Mundo_3/ex097.py
@@ -1,2 +1,12 @@
 # Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma
 # mensagem com tamanho adaptável.   Ex.: escreva('Olá, mundo!') Saída: __ Olá mundo!--  ('~')
+def escreva(txt):
+    tam = len(txt) + 4
+    print('-' * tam)
+    print(f'  {txt}')
+    print('-' * tam)
+
+
+escreva('SER DEV É SE COMPROMETER')
+escreva('TAMO NA LUTA')
+escreva('BIT')
