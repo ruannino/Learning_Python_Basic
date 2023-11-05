5610b6b7d77bb7676c0d093e5154ebc724021857
ruannino <ruanfariasdc@hotmail.com>
2023-11-05 12:37:56 -0300

    Forty Ninth Commit 05.11.2023

--- Mundo_3/ex091.py
+++ Mundo_3/ex091.py
@@ -1,2 +1,4 @@
 # crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
 # Dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+from random import randint
+dado = randint(1, 6)
