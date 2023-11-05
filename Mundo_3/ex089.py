6d0d6883efbd382230f81cd87679c4ad40e1f55e
ruannino <ruanfariasdc@hotmail.com>
2023-11-04 22:06:38 -0300

    Forty Eighth Commit 04.11.2023

--- Mundo_3/ex089.py
+++ Mundo_3/ex089.py
@@ -11,15 +11,15 @@ while True:
     nome = str(input('Nome do Aluno(a): ')).strip()
     aluno.append(nome)
     while True:
-        nota1 = float(input('Nota #1: '))
-        if 0 >= nota1 <= 10:
+        nota1 = float(input('Nota #1: ').replace(',', '.'))
+        if 0 <= nota1 <= 10:
             break
         else:
             print('\033[31mErro!\033[m Digite uma nota válida entre \033[33m0\033[m e \033[33m10\033[m...')
     aluno.append(nota1)
     while True:
-        nota2 = float(input('Nota #2: '))
-        if 0 >= nota2 <= 10:
+        nota2 = float(input('Nota #2: ').replace(',', '.'))
+        if 0 <= nota2 <= 10:
             break
         else:
             print('\033[31mErro!\033[m Digite uma nota válida entre \033[33m0\033[m e \033[33m10\033[m...')
@@ -41,11 +41,11 @@ print('\033[34m=\033[m' * 50)
 for a in all_class:
     print(f'\033[33m#{indice} \033[34mAluno: \033[m{a[0]:<18}', end='')
     if a[3] >= 7:
-        print(f'\033[34mMédia: \033[32m{a[3]:.2f}\033[m')
+        print(f'\033[34mMédia: \033[32m{a[3]:.1f}\033[m')
     elif 5 <= a[3] < 7:
-        print(f'\033[34mMédia: \033[33m{a[3]:.2f}\033[m')
+        print(f'\033[34mMédia: \033[33m{a[3]:.1f}\033[m')
     elif a[3] < 5:
-        print(f'\033[34mMédia: \033[31m{a[3]:.2f}\033[m')
+        print(f'\033[34mMédia: \033[31m{a[3]:.1f}\033[m')
     print()
     indice += 1
 print('\033[34m=\033[m' * 50)
@@ -73,17 +73,17 @@ while True:
         print('\033[34m=\033[m' * 50)
         print(f'\033[34mAluno(a):\033[m {all_class[notas][0]}')
         if all_class[notas][1] >= 7:
-            print(f'\033[34mNota 1\033[m --->  \033[32m{all_class[notas][1]:.2f}\033[m')
+            print(f'\033[34mNota 1\033[m --->  \033[32m{all_class[notas][1]:.1f}\033[m')
         elif 5 <= all_class[notas][1] < 7:
-            print(f'\033[34mNota 1\033[m --->  \033[33m{all_class[notas][1]:.2f}\033[m')
+            print(f'\033[34mNota 1\033[m --->  \033[33m{all_class[notas][1]:.1f}\033[m')
         elif all_class[notas][1] < 5:
-            print(f'\033[34mNota 1\033[m --->  \033[31m{all_class[notas][1]:.2f}\033[m')
+            print(f'\033[34mNota 1\033[m --->  \033[31m{all_class[notas][1]:.1f}\033[m')
 
         if all_class[notas][2] >= 7:
-            print(f'\033[34mNota 1\033[m --->  \033[32m{all_class[notas][2]:.2f}\033[m')
+            print(f'\033[34mNota 1\033[m --->  \033[32m{all_class[notas][2]:.1f}\033[m')
         elif 5 <= all_class[notas][2] < 7:
-            print(f'\033[34mNota 1\033[m --->  \033[33m{all_class[notas][2]:.2f}\033[m')
+            print(f'\033[34mNota 1\033[m --->  \033[33m{all_class[notas][2]:.1f}\033[m')
         elif all_class[notas][2] < 5:
-            print(f'\033[34mNota 1\033[m --->  \033[31m{all_class[notas][2]:.2f}\033[m')
+            print(f'\033[34mNota 1\033[m --->  \033[31m{all_class[notas][2]:.1f}\033[m')
         print('\033[34m=\033[m' * 50)
 print(f'{"FIM":x^50}')
