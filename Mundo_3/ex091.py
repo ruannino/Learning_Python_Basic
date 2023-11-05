6bd0a04a4d3881d2820dcdbc316c5a85806d13d1
ruannino <ruanfariasdc@hotmail.com>
2023-11-04 23:34:25 -0300

    Forty Eighth Commit 04.11.2023

--- Mundo_3/ex091.py
+++ Mundo_3/ex091.py
@@ -1,25 +1,2 @@
 # crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
 # Dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
-from random import randint
-from time import sleep
-from operator import itemgetter
-
-jogo = {'jogador1': randint(1, 6),
-        'jogador2': randint(1, 6),
-        'jogador3': randint(1, 6),
-        'jogador4': randint(1, 6)}
-ranking = []
-print('Jogando Dados...')
-print('-' * 30)
-sleep(1)
-
-for k, v in jogo.items():
-    print(f'O {k} tirou {v}')
-    sleep(1)
-print('{:-^30}'.format('RESULTADO'))
-sleep(1)
-ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
-for i, v in enumerate(ranking):
-    print(f'O {i + 1}º lugar foi o {v[0]} com {v[1]}')
-    sleep(1)
-print('=' * 30)
--- Mundo_3/ex093.py
+++ Mundo_3/ex093.py
@@ -1,30 +1,3 @@
 # Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e
 # quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será
 # guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
-somag = 0
-jogador = dict()
-jogador['nome'] = str(input('Nome do jogador: '))
-n = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
-gols = list()
-for g in range(0, n):
-    gols.append(int(input(f'Quantos gols o na partida {g + 1}: ')))
-    somag += gols[g]
-jogador['partidas'] = n
-jogador['gols'] = gols
-jogador['total'] = somag
-print('=-' * 15)
-print('{:^30}'.format('TABELA'))
-print('=-' * 15)
-print(f'O jogador {jogador["nome"]} jogou {jogador["partidas"]} partidas')
-print()
-for p, gp in enumerate(jogador['gols']):
-    print(f'   => Na partida {p + 1}, fez {gp}')
-print()
-print(f'Foi um total de {jogador["total"]} gols. ')
-if jogador['total'] / jogador['partidas'] > 3:
-    print(f'O jogador {jogador["nome"]} é FODÃO!')
-elif jogador['total'] / jogador['partidas'] >= 1:
-    print(f'O jogador {jogador["nome"]} está dentro da média')
-else:
-    print(f'O jogador {jogador["nome"]} tá ferrado!')
-print('=-' * 15)
