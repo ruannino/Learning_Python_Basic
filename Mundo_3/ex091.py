28687135e9754318c66b500c7cb634825b2bdc05
ruannino <ruanfariasdc@hotmail.com>
2023-11-05 12:55:35 -0300

    Forty Ninth Commit 05.11.2023

--- Mundo_3/ex091.py
+++ Mundo_3/ex091.py
@@ -1,4 +1,25 @@
 # crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um
 # Dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 from random import randint
-dado = randint(1, 6)
+from time import sleep
+from operator import itemgetter
+jogo = {'jogador1': randint(1, 6),
+        'jogador2': randint(1, 6),
+        'jogador3': randint(1, 6),
+        'jogador4': randint(1, 6)}
+ranking = list()
+print('\033[34m=+\033[m' * 15)
+print('JOGANDO DADOS'.center(30))
+print('\033[34m=+\033[m' * 15)
+for k, v in jogo.items():
+    print(f'O {k} tirou {v} no dado.')
+    sleep(1)
+print('\033[34m=+\033[m' * 15)
+print('\033[34m=+\033[m' * 15)
+print('RANKING'.center(30))
+print('\033[34m=+\033[m' * 15)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+for i, n in enumerate(ranking):
+    print(f'{i + 1}ª lugar > {n[0]} com {n[1]}.')
+    sleep(1)
+print('\033[34m=+\033[m' * 15)
