0046067ee92e27bab16e280c340da59a5b88f971
ruannino <ruanfariasdc@hotmail.com>
2023-11-05 20:04:53 -0300

    Forty Ninth Commit 05.11.2023

--- Mundo_3/ex098.py
+++ Mundo_3/ex098.py
@@ -5,12 +5,32 @@ from time import sleep
 
 
 def contador(i, f, p):
+    if p < 0:
+        p *= -1
     if p == 0:
         p = 1
-    for c in range(i, f + 1, p):
-        print(f'{c} -> ', end='')
-        sleep(1)
-    print('FIM!')
+    print('-' * 40)
+    print(f'Contagem de {i} até {f} de {p} em {p}!')
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='')
+            sleep(0.2)
+            cont += p
+        print('FIM!')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='')
+            sleep(0.2)
+            cont -= p
+        print('FIM!')
 
-contador(0, 10, 0)
+
+contador(0, 10, 1)
 contador(10, 0, 2)
+print('Agora é sua vez...')
+inicio = int(input('Ínicio: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(inicio, fim, passo)
