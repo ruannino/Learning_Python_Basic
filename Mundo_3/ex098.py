a47767097fb34461d0ba24f237a1fff27185c395
ruannino <ruanfariasdc@hotmail.com>
2023-11-05 19:20:16 -0300

    Forty Ninth Commit 05.11.2023

--- Mundo_3/ex098.py
+++ Mundo_3/ex098.py
@@ -1,15 +1,16 @@
 # Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo e realize
-# a contragem. Seu programa tem que realizar três contagens através da função criada:
+# a contagem. Seu programa tem que realizar três contagens através da função criada:
 # A) De 1 até 10, de 1 em 1.  B) De 10 até 0, de 2 em 2.  C) Uma contagem personalizada.
+from time import sleep
 
 
-contador(1, 10, 1)
-lin()
+def contador(i, f, p):
+    if p == 0:
+        p = 1
+    for c in range(i, f + 1, p):
+        print(f'{c} -> ', end='')
+        sleep(1)
+    print('FIM!')
+
+contador(0, 10, 0)
 contador(10, 0, 2)
-lin()
-print('Agora é a sua vez de personalizar a contagem: ')
-ini = int(input('Inicio: '))
-fim = int(input('Fim:    '))
-pas = int(input('Passo:  '))
-contador(ini, fim, pas)
-lin()
--- Mundo_3/ex099.py
+++ Mundo_3/ex099.py
@@ -1,2 +1,21 @@
 # Faça um programa que tenha uma funcão chamada maior(), que receba vários parâmetros com valores inteiros.
 # Seu programa tem que analizar todos os valores e dizer qual deles é o maior.
+def maior(* num):
+    cont = top = 0
+    for nu in num:
+        if cont == 0:
+            top = nu
+            cont += 1
+        elif cont >= 1:
+            if nu > top:
+                top = nu
+            cont += 1
+    print(f'Recebi os números ', end='')
+    for n in num:
+        print(f'\033[33m{n}\033[m ', end='')
+    print(f', totalizando \033[33m{cont}\033[m números e o maior foi o \033[33m{top}\033[m !')
+
+
+maior(10, 8, 9, 13, 6)
+maior(8, 2, 983, -4, 3)
+maior(-3, -6, -9, -290)
