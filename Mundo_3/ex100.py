04b7fad4198f3d0884f8e7ffad3fc12927ce9c6b
ruannino <ruanfariasdc@hotmail.com>
2023-11-06 20:34:43 -0300

    Fiftieth Commit 06.11.2023

--- Mundo_3/ex100.py
+++ Mundo_3/ex100.py
@@ -2,23 +2,53 @@
 # vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores
 # PARES sorteados pela função anterior.
 from random import randint
-
+from time import sleep
 
 def sorteia(lista):
-    print('Sorteando 5 valores para lista: ', end='')
+    print('\033[34m=\033[m' * 40)
+    print(f'SORTEANDO 05 NÚMEROS'.center(40))
+    print('\033[34m=\033[m' * 40)
+    print('Valores sorteados ', end='')
+    c = 3
+    while c != 0:
+        sleep(0.5)
+        print('\033[33m>\033[m', end=' ')
+        c -= 1
     for cont in range(0, 5):
+        sleep(0.5)
         valor = randint(1, 10)
         lista.append(valor)
-        print(f'{valor} ', end='')
+        print(f'\033[32m{valor}\033[m ', end='')
+    sleep(0.5)
     print('FIM!')
+    sleep(0.5)
+    print('\033[34m=\033[m' * 40)
 
 
 def soma_par(lst):
+    sleep(2)
+    print('\033[35m=\033[m' * 40)
+    print('SOMANDO PARES DE UMA LISTA'.center(40))
+    print('\033[35m=\033[m' * 40)
+    print('Analisando os números...')
+    c = 3
+    while c != 0:
+        sleep(0.5)
+        print('\033[31m> \033[m', end='')
+        c -= 1
     soma = 0
     for n in lst:
+        sleep(0.4)
         if n % 2 == 0:
+            print(f'\033[32m{n}\033[m ', end='')
             soma += n
-    print(f'A soma dos valores pares da lista {lst} é {soma}.')
+        else:
+            print(f'\033[31m{n}\033[m ', end='')
+    sleep(0.5)
+    print(f'\n\033[40mRESULTADO ->', end='')
+    sleep(0.5)
+    print(f'{soma:>25}\033[m')
+    print('\033[35m=\033[m' * 40)
 
 
 numbers = list()
