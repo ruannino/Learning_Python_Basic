6fb50e2215eb9b0110809d0854b04a54612585d2
ruannino <ruanfariasdc@hotmail.com>
2023-11-09 18:04:13 -0300

    Fifty Second Commit 09.11.2023

--- Mundo_3/ex103.py
+++ Mundo_3/ex103.py
@@ -1,18 +1,3 @@
 # Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e
 # quantos gols ele marcou.
 # O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
-
-
-def ficha(jogador='<desconhecido>', gols=0):
-    print('-=' * 23)
-    print(f'O Jogador {jogador} fez {gols} gols no campeonato!')
-    print('=-' * 23)
-
-
-jog = str(input('Nome do jogador: '))
-gol = input('Nº de gols: ')
-if gol.isnumeric():
-    gol = int(gol)
-else:
-    gol = 0
-ficha(jog, gol)
--- Mundo_3/ex104.py
+++ Mundo_3/ex104.py
@@ -1,19 +1,2 @@
 # Crie um programa que tenha uma função chamada leiaInt(), que vai funcionar semelhante a função input() do Python, só
 # que fazendo a validação para aceitar apenas um valor numérico.    Ex.: n = leiaInt('Digite um n')
-def leia_int(msn):
-    valor = 0
-    ok = False
-    while True:
-        num = str(input(msn))
-        if num.isnumeric():
-            valor = num
-            ok = True
-        else:
-            print('\033[31mErro! Digite um número inteiro válido...\033[m')
-        if ok:
-            break
-    return valor
-
-
-n = leia_int('Digite um número: ')
-print(f'Você digitou o número {n}.')
