13d0eaf4446ac8d400f0c5ea7dd0ec7906b3a43f
ruannino <ruanfariasdc@hotmail.com>
2023-11-10 00:18:14 -0300

    Fifty Third Commit 10.11.2023

--- Mundo_3/ex108.py
+++ Mundo_3/ex108.py
@@ -1,13 +1,2 @@
 # Adapte o código do desafio 107, criando uma função adicional chamada 'moeda()' que consiga mostrar os valores
 # como um valor monetário formatado.
-from utilidadesCeV import moeda
-
-v = float(input('Digite o valor do produto: R$ '))
-p = int(input('Digite a porcentagem a incrementar: % '))
-print(f'O valor do produto é {moeda.moeda(v)} com incremento de {p}% tem o valor final de '
-      f'{moeda.moeda(moeda.aumentar(v, p))}')
-print(f'O valor do produto é {moeda.moeda(v)} com desconto de {p}% tem o valor final de '
-      f'{moeda.moeda(moeda.diminuir(v, p))}')
-print(f'O dobro do valor é {moeda.moeda(moeda.dobro(v))}')
-print(f'A metade do valor é {moeda.moeda(moeda.metade(v))}')
-print(f'{moeda.moeda(v)}')
--- Mundo_3/ex112.py
+++ Mundo_3/ex112.py
@@ -1,8 +1,3 @@
 # Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado.
 # Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função input(), mas com uma validação
 # de dados para aceitar apenas valores que sejam monetários.
-from utilidadesCeV import moeda
-from utilidadesCeV import dado
-
-v = dado.leia_dinheiro('Digite o valor do produto: R$ ')
-moeda.resumo(v, 25, 22)
