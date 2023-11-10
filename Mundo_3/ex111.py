d2303021e2a078fdadc027755e797228df06969c
ruannino <ruanfariasdc@hotmail.com>
2023-11-10 02:23:31 -0300

    Fifty Third Commit 10.11.2023

--- Mundo_3/ex111.py
+++ Mundo_3/ex111.py
@@ -1,3 +1,7 @@
 # Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
 # Transfira todas as funções utilizadas nos desafios 107, 108, 109 e 110 para o primeiro pacote e mantenha tudo
 # funcionando.
+from utilidadesCeV import moeda
+
+n = float(input('Digite o preço: R$'))
+moeda.resumo(n, 10, 15)
--- Mundo_3/ex112.py
+++ Mundo_3/ex112.py
@@ -1,3 +1,8 @@
 # Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado.
 # Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função input(), mas com uma validação
 # de dados para aceitar apenas valores que sejam monetários.
+from utilidadesCeV.moeda import resumo
+from utilidadesCeV.dado import leia_dinheiro
+
+n = leia_dinheiro('Digite o preço: R$')
+resumo(n, 10, 15)
