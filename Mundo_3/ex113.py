698ba249d3e97f3be23c7fa0b4c5f4d4337a7ed2
ruannino <ruanfariasdc@hotmail.com>
2023-11-10 20:16:02 -0300

    Fifty Third Commit 10.11.2023

--- Mundo_3/ex113.py
+++ Mundo_3/ex113.py
@@ -1,3 +1,34 @@
 # Reescreva a função leia_int() que fizemos no desafio 104, incluindo agora a possibilidade
 # da digitação de um número de tipo inválido. Aproveite e crie também uma função leia_float()
 # com a mesma funcionalidade.
+def leia_int(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mErro!\033[m Digite um número inteiro válido!')
+            continue
+        except KeyboardInterrupt:
+            print('\033[33mEntrada de dados interropida pelo usuário!\033[m')
+            return 0
+        else:
+            return n
+
+
+def leia_float(txt):
+    while True:
+        try:
+            num = float(input(txt))
+        except (TypeError, ValueError):
+            print('\033[31mErro!\033[m Digite um número real válido!')
+            continue
+        except KeyboardInterrupt:
+            print('\033[33mEntrada de dados interropida pelo usuário!\033[m')
+            return 0
+        else:
+            return num
+
+
+num = leia_int('Digite um valor inteiro: ')
+n = leia_float('Digite um valor real: ')
+print(f'O valor inteiro digitado foi {num} e o valor real foi {n}')
