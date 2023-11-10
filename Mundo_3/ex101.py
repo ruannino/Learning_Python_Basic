bcaeb347de4f194df1c12fad017a2fe175d77ce1
ruannino <ruanfariasdc@hotmail.com>
2023-11-09 22:02:49 -0300

    Fifty Second Commit 09.11.2023

--- Mundo_3/ex101.py
+++ Mundo_3/ex101.py
@@ -1,2 +1,27 @@
 # crie um programa que tenha uma função chamada voto() que vai receber como parâmentro o ano de nascimento de uma
 # pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
+def voto(a):
+    """
+    -> Verifica a idade do usuário e retorna a situação de voto.
+    :param a: Ano de nascimento do usuário.
+    :return: Retorna a situação conforme a idade do usuário. [Negado, Opcional ou Obrigatório]
+    Criada por Ruannino.
+    """
+    from datetime import date
+    atual = date.today().year
+    idade = atual - a
+    if idade < 16:
+        return f'Um cidadão com {idade} anos | Voto: \033[31mNegado!\033[m'
+    elif 16 <= idade < 18 or idade >= 65:
+        return f'Um cidadão com {idade} anos | Voto: \033[32mOpcional!\033[m'
+    else:
+        return f'Um cidadão com {idade} anos | Voto: \033[33mObrigatório!\033[m'
+
+
+# Programa Principal
+print('\033[32m=\033[33m=\033[m' * 20)
+print(f'\033[32m{"SISTEMA DE VOTO":^40}\033[m')
+print('\033[32m=\033[33m=\033[m' * 20)
+nasc = int(input('Qual seu ano de nascimento ? '))
+print(voto(nasc))
+print('\033[32m=\033[33m=\033[m' * 20)
--- Mundo_3/ex106.py
+++ Mundo_3/ex106.py
@@ -1,2 +1,9 @@
 # Faça um mini-sistema que utilize o INTERACTIVE HELP do Python. O usuário vai digitar o comando e o manual vai
 # aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará.    OBS.: use cores.
+cor = ('\033[m',         # 0 - sem cores
+       '\033[0;30;41m',  # 1 - vermelha
+       '\033[0;30;42m',  # 2 - verde
+       '\033[0;30;43m',  # 3 - amarelo
+       '\033[0;30;44m',  # 4 - azul
+       '\033[0;30;45m',  # 5 - roxo
+       '\033[7;30m')     # 6 - branco
