38985d711f118315435828c7105efbc1afb14e32
ruannino <ruanfariasdc@hotmail.com>
2023-11-10 20:31:48 -0300

    Fifty Third Commit 10.11.2023

--- Mundo_3/ex114.py
+++ Mundo_3/ex114.py
@@ -1,1 +1,11 @@
 # Crie um código em Python que teste se o site Pudim está acessível pelo computador usado.
+# print(site.read()) lê os dados do site
+import urllib.error
+import urllib.request
+
+try:
+    urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('\033[31mO site pudim não está acessível no momento!\033[m')
+else:
+    print('\033[32mConsegui acessar o site pudim!\033[m')
